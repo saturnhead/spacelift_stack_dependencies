cca13473bd22d59fc5ce434323f16157725c6da5
Flavius <flaviuscdinu93@gmail.com>
2025-04-09 16:18:36 +0300

    added ansible code

--- configurations/network/variables.tf
+++ configurations/network/variables.tf
@@ -17,7 +17,6 @@ variable "subnets" {
   type = map(object({
     vpc_key                 = string
     cidr_block              = string
-    availability_zone       = string
     map_public_ip_on_launch = bool
     tags                    = map(string)
   }))
